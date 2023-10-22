5127e68658d4f04b3bc9f4a753025e8cc32b942b
Nikolay <lemoor@mail.ru>
2023-10-22 07:21:17 +0400

    new

--- src/js/background.js
+++ src/js/background.js
@@ -1,18 +1,30 @@
 const resumeData = {}
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    if (message.resumeData) {
-        resumeData.gender = message.resumeData.gender;
-        resumeData.birthday = message.resumeData.birthday;
-  
-        chrome.storage.local.set({ 'gender': resumeData.gender }, function() {
-            console.log('Gender saved:', resumeData.gender);
-        });
 
-        chrome.storage.local.set({ 'birthday': resumeData.birthday }, function() {
-            console.log('Birthday saved:', resumeData.birthday);
-        });
+    if(message.down == true){
+        chrome.tabs.query({ active: true, currentWindow: true }, async function (tabs) {
+            chrome.scripting.executeScript({
+                target: { tabId: tabs[0].id },
+                function: () => {
+                    const resume = {}
+                    const res = document.querySelector('.resume-applicant')
 
+                    const genderElement = res.querySelector('[data-qa="resume-personal-gender"]');
+                    resume.gender = genderElement ? genderElement.innerText : "Unknown";
+
+                    const birthdayElement = res.querySelector('[data-qa="resume-personal-birthday"]');
+                    resume.birthday = birthdayElement ? birthdayElement.innerText : "Unknown";
+                    
+                    return { resume };
+                }
+            }).then((result) => {
+
+                chrome.storage.local.set({ 'gender': result[0].result.resume.gender });
+                chrome.storage.local.set({ 'birthday': result[0].result.resume.birthday });
+
+            });
+        });
     }
     if(message.fill == true){
         chrome.tabs.query({ active: true, currentWindow: true }, async function(tabs) {
@@ -33,10 +45,7 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
                         birthdayInput.value = result.birthday;
 
                     });
-
                     // chrome.storage.local.clear();
-
-                    
                 },
                 args: [resumeData],
             })
@@ -46,4 +55,71 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
     if(message.clear == true){
         chrome.storage.local.clear();
     }
+    if(message.page == true){
+        const url = "http://127.0.0.1:5500/src/html/form.html"                
+        chrome.tabs.create({ url });
+    }
 });
+
+chrome.runtime.onInstalled.addListener(function() {
+    chrome.commands.onCommand.addListener(function(command) {
+      if (command === "downloadCommand") {
+        chrome.tabs.query({ active: true, currentWindow: true }, async function (tabs) {
+            chrome.scripting.executeScript({
+                target: { tabId: tabs[0].id },
+                function: () => {
+                    const resume = {}
+                    const res = document.querySelector('.resume-applicant')
+
+                    const genderElement = res.querySelector('[data-qa="resume-personal-gender"]');
+                    resume.gender = genderElement ? genderElement.innerText : "Unknown";
+
+                    const birthdayElement = res.querySelector('[data-qa="resume-personal-birthday"]');
+                    resume.birthday = birthdayElement ? birthdayElement.innerText : "Unknown";
+                    
+                    return { resume };
+                }
+            }).then((result) => {
+
+                chrome.storage.local.set({ 'gender': result[0].result.resume.gender });
+                chrome.storage.local.set({ 'birthday': result[0].result.resume.birthday });
+
+            });
+        });
+      } else if (command === "pageCommand"){
+
+        const url = "http://127.0.0.1:5500/src/html/form.html"                
+        chrome.tabs.create({ url });
+
+      } else if (command === "fillCommand") {
+
+        chrome.tabs.query({ active: true, currentWindow: true }, async function (tabs) {
+
+            chrome.scripting.executeScript({
+                target: { tabId: tabs[0].id },
+                function: (args) => {
+                    
+                    chrome.storage.local.get('gender', function(result) {
+
+                        const genderInput = document.querySelector('.gender');
+                        genderInput.value = result.gender;
+
+                    });
+
+                    chrome.storage.local.get('birthday', function(result) {
+
+                        const birthdayInput = document.querySelector('.birthday');
+                        birthdayInput.value = result.birthday;
+
+                    });
+                    // chrome.storage.local.clear();
+                },
+                args: [resumeData],
+            })
+
+        });
+      } else if (command === "clearCommand") {
+        chrome.storage.local.clear();
+      }
+    });
+  });
--- src/js/content.js
+++ src/js/content.js
@@ -13,3 +13,32 @@
 
 // genderInput.value = resumeData.gender;
 // birthdayInput.value = resumeData.birthday;
+
+// document.addEventListener('DOMContentLoaded', function() {
+    // ваш JavaScript-код здесь
+
+
+
+const asideCta = document.querySelector('.aside-cta');
+if (asideCta) {
+    const link = document.createElement('a');
+    link.href = '#'; 
+    link.innerText = 'Моя ссылка'; 
+
+    link.classList.add('ws-nowrap', 's-btn', 's-btn__primary');
+
+    asideCta.appendChild(link);
+}
+
+
+const blockButton = document.querySelector('.bloko-button-group');
+if (blockButton) {
+    const button = document.createElement('button');
+    button.innerText = 'Моя кнопка'; 
+
+    button.classList.add('bloko-button');
+
+    blockButton.appendChild(button);
+}
+
+// });
